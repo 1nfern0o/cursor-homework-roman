09c460bd90a97b13e1c34500622db2fa0c97e832
Roman <romantcvetkov@gmail.com>
2020-08-07 18:58:12 +0300

    add 5 functions

--- 03-homework/main.js
+++ 03-homework/main.js
@@ -1,11 +1,10 @@
 // 1. Створити функцію getMaxDigit(number) – яка отримує будь-яке число та виводить найбільшу цифру в цьому числі. Приклади: 1236 -> 6, 987 -> 9, 385 -> 8
 
-function getMaxDigit() {
-    let number = 251454873656;
+function getMaxDigit(number) {
     maxNumeral = number.toString().split('');
     return Math.max(...maxNumeral);
 }
-console.log(getMaxDigit());
+console.log(getMaxDigit(251454873656));
 
 
 // 2. Створити функцію, яка визначає ступінь числа. Не використовуючи Math.pow та **. Використовуйте цикл
@@ -63,3 +62,95 @@ function countLetter(letter, word) {
     return result;
 }
 console.log(countLetter('а', 'Асталавіста'));
+
+
+// 7. Створіть функцію, яка конвертує долари в гривні та навпаки в залежності від наявності символа $ або UAH в рядку. Приклад: convertCurrency("100$") -> 2500 грн. або convertCurrency("2500UAH") -> 100$
+// Врахуйте, інші валюти не конвертуються, потрібно виводити помилку, і також регістр uah не має значення.
+
+function convertCurrency() {
+    let currency = prompt('Введите сумму с "uah" или "$"').toLowerCase(),
+        currencyDollars = ' $',
+        currencyUAH = ' UAH',
+        rate = 27.5,
+        totalSum = null;
+
+    if (currency.indexOf('$') != -1) {
+        totalSum = (parseInt(currency) * rate).toFixed(2) + currencyUAH;
+    } else if (currency.indexOf('uah') != -1 ) {
+        totalSum = (parseInt(currency) / rate).toFixed(2) + currencyDollars;
+    } else {
+        totalSum = 'такую валюту мы не меняем, введите uah или $';
+        prompt('Введите сумму с "uah" или "$"');
+    }
+    return totalSum;
+}
+console.log(convertCurrency());
+
+// 8. Створіть функцію генерації випадкового паролю (тільки числа), довжина встановлюється користувачем або по замовчуванню = 8 символам.
+// Приклад: getRandomPassword(4) -> 1875, getRandomPassword() -> 87240124
+
+
+
+function getRandomPassword(lengthPassword) {
+    let randomPassword = '',
+        lengthDefault = 8,
+        password = 0;
+    if (lengthPassword == 0) {
+
+        for (let i = 0; i < lengthDefault; i++) {
+            password = Math.floor(Math.random() * 10);
+            randomPassword = randomPassword + +password;
+        }
+
+    } else {
+
+        for (let i = 0; i < lengthPassword; i++) {
+            password = Math.floor(Math.random() * 10);
+            randomPassword = randomPassword + +password;
+        }
+
+    }
+    return randomPassword;
+}
+
+console.log(getRandomPassword(8));
+
+// 9. Створіть функцію, яка видаляє всі букви з речення. Приклад: deleteLetters('a', "blablabla") -> "blblbl"
+
+function deleteLetters(letter, word) {
+    let result = '';
+    for (let i = 0; i < word.length; i++) {
+        if (letter !== word[i]) result = result + word[i];
+    }
+    return result;
+}
+
+console.log(deleteLetters('b', 'blablabla'));
+
+// 10. Створіть функцію, яка перевіряє, чи є слово паліндромом. Приклад: isPalyndrom("мадам") -> true, isPalyndrom("кокос") -> false, isPalyndrom("Я несу гусеня") -> true
+
+function isPalyndrom(word) {
+    let wordFormat = '';
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === ' ') continue;
+        else wordFormat += word[i].toLowerCase();
+    }
+    for (let i = wordFormat.length - 1, j = 0; i >= 0 && j < wordFormat.length - 1; i--, j++) {
+        if (wordFormat[i] !== wordFormat[j]) return false;
+    }
+    return true;
+}
+
+console.log(isPalyndrom('Я несу гусеня'));
+
+// 11. Створіть функцію, яка видалить з речення букви, які зустрічаються більше 1 разу. Приклад: deleteDuplicateLetter("Бісквіт був дуже ніжним") -> "сктдеим"
+
+function deleteDuplicateLetter(word) {
+    let letterNoRepeat = ' ';
+    for (let i = 0; i < word.length; i++) {
+        if (word.lastIndexOf(word[i]) == word.indexOf(word[i])) letterNoRepeat += word[i];
+    }
+    return letterNoRepeat;
+}
+
+console.log(deleteDuplicateLetter('Саламон Гранди и Двуликий'));
